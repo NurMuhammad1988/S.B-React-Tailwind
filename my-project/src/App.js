ddffe531e42670ef226e7662058e4a9fe80838c3
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-02-10 04:02:35 +0500

    its so slowly

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
-
+import { styles } from "./util/style";
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline font-montserrat ">
-            Hello world!
-        </h1>
+        <div className="bg-primary w-full overflow-hidden ">
+            <div
+                className={`${styles.paddingX} ${styles.flexCenter}`}
+            >
+                <div className={`${styles.container} `}>
+                    <div className={`${styles.heading}`}>Navbar</div>
+                </div>
+            </div>
+
+            {/* <div className="bg-slate-500">
+                <p className={`${styles.paragraph}`}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Cupiditate optio, accusantium enim aspernatur autem ad
+                    perspiciatis consectetur recusandae? Quaerat, itaque!
+                </p>
+            </div> */}
+        </div>
     );
-}; 
+};
 
 export default App;
--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,5 +1,3 @@
-
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
@@ -10,10 +8,9 @@ module.exports = {
                 secondary: "#00f6ff",
                 lightWhite: "rgba(255, 255, 255, 0.7)",
                 lightBlue: "rgba(9, 151, 124, 0.1)",
-
             },
             fontFamily: {
-              montserrat:["Montserrat","sans-serif"],
+                montserrat: ["Montserrat", "sans-serif"],
             },
         },
         screens: {
